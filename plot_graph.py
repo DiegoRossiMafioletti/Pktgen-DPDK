150d67e492f977c0e87fccb704d91bd04011edc2
DiegoRossiMafioletti <diego.rossi@ifes.edu.br>
2022-07-07 19:05:44 -0300

    add python for plotting

--- plot_graph.py
+++ plot_graph.py
@@ -1,74 +1,95 @@
+#!/usr/bin/python3
+
 from itertools import count
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+# import cv2
 from matplotlib.animation import FuncAnimation
 import paramiko
 
-plt.style.use('fivethirtyeight')
+plt_style='fivethirtyeight'
+#plt_style='fast'
+
+# remote/local
+plt_mode='remote'
+
+plt.style.use(plt_style)
 # refresh time, in secs
-interval = 500
+interval = 1000
+
+# img = cv2.imread('testbed.png') 
+# cv2.imshow('Testbed', img)
 
 plot_style='line'
 
-HOST="localhost"
-PORT=2522
-USERNAME="root"
-ID_RSA_FILE="/home/diego/.ssh/id_rsa"
-LOGFILE="/root/logfile.txt"
+LOGFILE="/tmp/logfile.txt"
+if (plt_mode == 'remote'):
+    HOST="localhost"
+    PORT=2522
+    USERNAME="root"
+    ID_RSA_FILE="/home/diego/.ssh/id_rsa"
 
-sftp_key = paramiko.RSAKey.from_private_key_file(ID_RSA_FILE)
-transport = paramiko.Transport((HOST, PORT))
-transport.start_client(event=None, timeout=15)
-transport.get_remote_server_key()
-transport.auth_publickey(username=USERNAME, key=sftp_key)
-sftp = paramiko.SFTPClient.from_transport(transport) 
+    # SSH connection: remote testbed
+    sftp_key = paramiko.RSAKey.from_private_key_file(ID_RSA_FILE)
+    transport = paramiko.Transport((HOST, PORT))
+    transport.start_client(event=None, timeout=15)
+    transport.get_remote_server_key()
+    transport.auth_publickey(username=USERNAME, key=sftp_key)
+    sftp = paramiko.SFTPClient.from_transport(transport) 
 
 xs = []
 y1s = []
 y2s = []
-fig, (ax1, ax2) = plt.subplots(2)
+fig, (ax1, ax2) = plt.subplots(2, figsize=(10, 5))
 
 # index = count()
 
 
 def animate(i, xs:list, y1s:list, y2s:list):
-    #data = pd.read_csv('logfile.txt', delimiter = ';', comment = '#')
-    with sftp.open(LOGFILE, mode='r') as f:
-        f.prefetch()
-        data = pd.read_csv(f, delimiter = ';', comment = '#')
-        x = data['x_value']
-        y1 = data['sw_latency']
-        y1 = [y for y in y1 if y != 0]
-        y2 = data['hw_latency']
-        y2 = [y for y in y2 if y != 0]
+    if (plt_mode == 'local'):
+        data = pd.read_csv(LOGFILE, delimiter = ';', comment = '#')
+    elif (plt_mode == 'remote'):
+        with sftp.open(LOGFILE, mode='r') as f:
+            f.prefetch()
+            data = pd.read_csv(f, delimiter = ';', comment = '#')
 
-        plt.cla()
-        # Limit x and y lists to 20 items
-        xs = x[-20:]
-        y1s = y1[-20:]
-        y2s = y2[-20:]
-        ax1.clear()
-        ax2.clear()
-        fig.suptitle('Fast Intercept Demonstration: RTT Latency')
-        # Draw x and y lists
-        if (plot_style == 'bar'):
-            ax1.bar(xs, y1s, label='SW Latency', color='blue')
-            ax2.bar(xs, y2s, label='HW Latency', color='red')
-        else:
-            ax1.plot(xs, y1s, label='SW Latency', color='blue')
-            ax2.plot(xs, y2s, label='HW Latency', color='red')
+    x = data['x_value']
+    y1 = data['sw_latency']
+    # y1 = [y for y in y1 if y != 0]
+    y2 = data['hw_latency']
+    # y2 = [y for y in y2 if y != 0]
 
-        ax1.legend(loc='upper left')
-        ax2.legend(loc='upper left')
+    plt.cla()
+    # Limit x and y lists to 10 items
+    xs = x[-10:]
+    y1s = y1[-10:]
+    y2s = y2[-10:]
 
-        plt.xticks(rotation=45, ha='right')
-        plt.ylabel('Latency (µs)')
-        
-        plt.tight_layout()
+    if (len(y1s) < 10 or len(y2s) < 10):
+        return
 
+    ax1.clear()
+    ax2.clear()
+    fig.suptitle('Fast Intercept Demonstration: RTT Latency')
+    # Draw x and y lists
+    if (plot_style == 'bar'):
+        ax1.bar(xs, y1s, label='SW Latency', color='blue')
+        ax2.bar(xs, y2s, label='HW Latency', color='red')
+    else:
+        ax1.plot(xs, y1s, label='SW Latency', color='blue', marker='o')
+        ax2.plot(xs, y2s, label='HW Latency', color='red', marker='o')
+
+    ax1.legend(loc='upper left')
+    ax2.legend(loc='upper left')
+
+    # ax1.set_ylim(bottom=0)
+    # ax2.set_ylim(bottom=0)
+
+    # plt.xticks(rotation=45, ha='right')
+    plt.ylabel('Latency (µs)')
+    plt.xlabel('Sample number')
+    plt.tight_layout()
 
 ani1 = FuncAnimation(plt.gcf(), animate, fargs=(xs,y1s,y2s), interval=interval)
-
 plt.tight_layout()
 plt.show()
---
+++ plot_graph_remote.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+# import cv2
+from matplotlib.animation import FuncAnimation
+import paramiko
+
+plt_style='fivethirtyeight'
+#plt_style='fast'
+
+plt.style.use(plt_style)
+# refresh time, in secs
+interval = 1000
+
+# img = cv2.imread('testbed.png') 
+# cv2.imshow('Testbed', img)
+
+plot_style='line'
+
+HOST="localhost"
+PORT=3022
+USERNAME="root"
+ID_RSA_FILE="/home/diego/.ssh/id_rsa"
+LOGFILE="/root/logfile.txt"
+
+# SSH connection: remote testbed
+sftp_key = paramiko.RSAKey.from_private_key_file(ID_RSA_FILE)
+transport = paramiko.Transport((HOST, PORT))
+transport.start_client(event=None, timeout=15)
+transport.get_remote_server_key()
+transport.auth_publickey(username=USERNAME, key=sftp_key)
+sftp = paramiko.SFTPClient.from_transport(transport) 
+
+xs = []
+y1s = []
+y2s = []
+fig, (ax1, ax2) = plt.subplots(2, figsize=(10, 5))
+
+# index = count()
+
+
+def animate(i, xs:list, y1s:list, y2s:list):
+    #data = pd.read_csv('logfile.txt', delimiter = ';', comment = '#')
+    with sftp.open(LOGFILE, mode='r') as f:
+        f.prefetch()
+        data = pd.read_csv(f, delimiter = ';', comment = '#')
+        x = data['x_value']
+        y1 = data['sw_latency']
+        # y1 = [y for y in y1 if y != 0]
+        y2 = data['hw_latency']
+        # y2 = [y for y in y2 if y != 0]
+
+        plt.cla()
+        # Limit x and y lists to 10 items
+        xs = x[-10:]
+        y1s = y1[-10:]
+        y2s = y2[-10:]
+
+        if (len(y1s) < 10 or len(y2s) < 10):
+            return
+
+        ax1.clear()
+        ax2.clear()
+        fig.suptitle('Fast Intercept Demonstration: RTT Latency')
+        # Draw x and y lists
+        if (plot_style == 'bar'):
+            ax1.bar(xs, y1s, label='SW Latency', color='blue')
+            ax2.bar(xs, y2s, label='HW Latency', color='red')
+        else:
+            ax1.plot(xs, y1s, label='SW Latency', color='blue', marker='o')
+            ax2.plot(xs, y2s, label='HW Latency', color='red', marker='o')
+
+        ax1.legend(loc='upper left')
+        ax2.legend(loc='upper left')
+
+        # ax1.set_ylim(bottom=0)
+        # ax2.set_ylim(bottom=0)
+
+        # plt.xticks(rotation=45, ha='right')
+        plt.ylabel('Latency (µs)')
+        plt.xlabel('Sample number')
+        plt.tight_layout()
+
+ani1 = FuncAnimation(plt.gcf(), animate, fargs=(xs,y1s,y2s), interval=interval)
+plt.tight_layout()
+plt.show()
